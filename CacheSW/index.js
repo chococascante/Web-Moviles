34a2a2cfacd1f572d2e3110c22698fdba7f433cc
Luis Cascante <luisalonzocascante@gmail.com>
2021-09-22 18:00:42 -0600

    pwa

--- CacheSW/index.js
+++ CacheSW/index.js
@@ -6,13 +6,27 @@ async function registrarServiceWorker() {
 
   try {
     const registration = await navigator.serviceWorker.register(
-      "/service-worker.js"
+      "./service-worker.js"
     );
     console.log("Service worker registrado correctamente", registration.scope);
   } catch (error) {
     console.error(error);
   }
 }
+
+// PWA
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log("👍", "beforeinstallprompt", event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+});
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log("👍", "beforeinstallprompt", event);
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+});
 
 window.onload = () => {
   // registrar el worker
@@ -26,5 +40,20 @@ window.onload = () => {
     const imagen = document.createElement("img");
     imagen.src = urlIcono;
     document.querySelector("#lista-iconos").appendChild(imagen);
+  });
+
+  document.getElementById("boton-pwa").addEventListener("click", async () => {
+    console.log("Hola");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      console.log("No hay promptEvent");
+      // The deferred prompt isn't available.
+      return;
+    }
+    // Mostramos el prompt
+    promptEvent.prompt();
+    const result = await promptEvent.userChoice;
+    console.log("👍", "Elección", result);
+    window.deferredPrompt = null;
   });
 };
